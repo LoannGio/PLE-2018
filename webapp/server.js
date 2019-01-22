d8bbf138d0e57c0d497623ba3792c4b8c363ed5d
mmayolini <maxime.mayolini@gmail.com>
2019-01-22 16:10:41 +0100

    hbase client node

--- webapp/server.js
+++ webapp/server.js
@@ -3,6 +3,9 @@ const path = require('path');
 const app = express();
 const logger = require('morgan');
 const port = 1337;
+const hbase = require('hbase');
+
+let client = hbase({ host: '10.0.213.28', port: 8080 });
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -11,6 +14,9 @@ app.set('view engine', 'ejs');
 
 app.get('/', function (req, res){  //index
   res.render('index', {title: 'BigData Viewer'});
+  client.table('lgiovannange').row('N42E009.hgt').get('infos', function(err, cell){
+    console.log(cell);
+  });
 });
 
 app.get('/projet/creer', function (req, res){
